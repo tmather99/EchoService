9a6e2f0dd2ef854b233903aaa6402dba67c74256
Thanhy Mather <tmather@vmware.com>
2022-11-23 13:49:55 -0800

    namespace fixes

--- tcp_client/Properties/AssemblyInfo.cs
+++ tcp_client/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WCF-client-windows")]
+[assembly: AssemblyTitle("tcp_client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WCF-client-windows")]
+[assembly: AssemblyProduct("tcp_client")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
